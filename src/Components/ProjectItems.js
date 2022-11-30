958e1d7bad70f94b5c80882ef74b99afd1ddea7d
Mohammad Mohisn <mohsinmohammad122@gmail.com>
2022-11-30 23:50:57 +0530

    weather  app added

--- src/Components/ProjectItems.js
+++ src/Components/ProjectItems.js
@@ -14,16 +14,22 @@ const proj = [
       "Amazon Clone with add to cart/ remove from cart and sign in/out functionality. Generates random images, names and prices.",
     website: "https://mohsin-25.github.io/amazonclone/",
   },
-
   {
     id: 3,
+    title: "Weather App",
+    img: "images/weatherapp 3x2.jpg",
+    detail: "Planning your outings? Weather is something to keep in mind",
+    website: "https://mohsin-25.github.io/weatherapp/",
+  },
+  {
+    id: 4,
     title: "Calculator",
     img: "images/calculator 3x2.jpg",
     detail: "To make arithmetic calculations",
     website: "https://mohsin-25.github.io/calculator/",
   },
   {
-    id: 4,
+    id: 5,
     title: "To Do List",
     img: "images/todo list 3x2.jpg",
     detail:
@@ -31,22 +37,7 @@ const proj = [
     website: "https://mohsin-25.github.io/todolist/",
   },
   {
-    id: 5,
-    title: "Fb Login Clone",
-    img: "images/fbclone 3x2.jpg",
-    detail: "Facebook login clone (UI)",
-    website: "https://mohsin-25.github.io/fbclone/",
-  },
-  {
     id: 6,
-    title: "Mapty",
-    img: "images/mapty 3x2.jpg",
-    detail:
-      "Keep track of your workouts on the map. Just tap on the map where you've ran/cycled and enter some details.",
-    website: "https://mohsin-25.github.io/mapty/",
-  },
-  {
-    id: 7,
     title: "Guess My Number",
     img: "images/guess my name 3x2.jpg",
     detail:
@@ -54,13 +45,49 @@ const proj = [
     website: "https://mohsin-25.github.io/guessmynumber/",
   },
   {
-    id: 8,
+    id: 7,
     title: "Dice Game",
     img: "images/dice game 3x2.jpg",
     detail:
       "Try to score 20, Roll the dice, you can either hold the current score and add it to final score or roll again. If you get 1, you loose your turn and your final score becomes 0. Have fun!!",
     website: "https://mohsin-25.github.io/dicegame/",
   },
+  {
+    id: 8,
+    title: "Mapty",
+    img: "images/mapty 3x2.jpg",
+    detail:
+      "Keep track of your workouts on the map. Just tap on the map where you've ran/cycled and enter some details.",
+    website: "https://mohsin-25.github.io/mapty/",
+  },
+  // {
+  //   id: 5,
+  //   title: "Fb Login Clone",
+  //   img: "images/fbclone 3x2.jpg",
+  //   detail: "Facebook login clone (UI)",
+  //   website: "https://mohsin-25.github.io/fbclone/",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Bankist",
+  //   img: "images/bankist 3x2.jpg",
+  //   detail: "A banking homepage",
+  //   website: "https://mohsin-25.github.io/bankist/",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Color Flip",
+  //   img: "images/color flip 3x2.jpg",
+  //   detail: "Change color with click of a button",
+  //   website: "https://mohsin-25.github.io/colorflip/",
+  // },
+  // {
+  //   id: 5,
+  //   title: "First Portfolio Website",
+  //   img: "images/color flip 3x2.jpg",
+  //   detail: "Portfolio Website with HTML, CSS and JavaScript",
+  //   website: "https://mohsin-25.github.io/firstportfolio-website/",
+  // },
 ];
 
 export default proj;
